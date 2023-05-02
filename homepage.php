685cf2242d3e0a1a35a82ae95a57b811c4d521ee
MartoKartechnicata <marti170705@gmail.com>
2023-05-02 09:59:54 +0300

    aaaa

--- homepage.php
+++ homepage.php
@@ -13,30 +13,40 @@ if (!$connection) {
 
 session_start();
 
-if (isset($_SESSION['firstName']) && isset($_SESSION['lastName'])) {
+//if (isset($_SESSION['firstName']) && isset($_SESSION['lastName'])) {
 
- ?>
+?>
 
 <!DOCTYPE html>
 
 <html>
 
 <head>
-
     <title>USF-Home</title>
     <meta charset="UTF-8">
     <meta name="description" content="Home page of the USF - UKTC STUDENT FIGHTS">
     <meta name="keywords" content="usf, uktc, fights, mma, mma promotions">
     <meta name="author" content="Martin Yordanov 19315, Kristiyan Yordanov 19313, Stivan Borisov 19321">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <link rel="stylesheet" type="text/css" href="Style.css">
 </head>
 
 <body>
 
+<div class="container text-center">
+    <div class="row">
+        <header>
+            <img src="images/logo.png" class="img-fluid" alt="uktc logo" style="size:inherit;" style="height: 75px">
+        </header>
+    </div>
+</div>
 </body>
 
 </html>
 
 <?php 
 
-}
+//}
  ?>
