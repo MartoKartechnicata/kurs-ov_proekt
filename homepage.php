b0c7707223368ce45fc4cb8f1a9a95b04a3ba932
MartoKartechnicata <marti170705@gmail.com>
2023-05-22 19:33:24 +0300

    nz

--- homepage.php
+++ homepage.php
@@ -30,18 +30,20 @@ session_start();
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-    <link rel="stylesheet" type="text/css" href="Style.css">
+    <link rel="stylesheet" type="text/css" href="Style.css?v1=1234">
 </head>
 
 <body>
-
-<div class="container text-center">
+    <?php 
+    include "menu.html" 
+    ?>
+<!--<div class="container text-center">
     <div class="row">
         <header>
-            <img src="images/logo.png" class="img-fluid" alt="uktc logo" style="size:inherit;" style="height: 75px">
+            <img src="images/logo1.png" class="img-fluid" alt="usf logo" style="height:inherit;">
         </header>
     </div>
-</div>
+</div> -->
 </body>
 
 </html>
