0f8bf2f29537c2a76d3ea6397bfc570a46e76d3a
Kris1234567891011 <krisi170705@gmail.com>
2023-05-27 20:16:56 +0300

    dada

--- pages/aboutus.php
+++ pages/aboutus.php
@@ -40,23 +40,60 @@ session_start();
     ?>
   </header>
   <h1 class="text-center">About us</h1>
+  <hr noshade style="color:crimson">
 
   <div class="container-fluid">
     <div class="row">
-        <div class="col-lg-3 col-6">
-            <p>akfbkdbjsbjjbj<br>
-              ksd;jkjljfs;lj<br>
-            </p> 
+        <div class="col-lg-7 col-12">
+          <h3 class="crimsonbg whiteText text-center" style="padding-bottom:0.25%;">Our project</h3>
+          <br>
+          <div class="col-push-12 d-lg-block"
+            <p>We're a group of three students from the Vocational School of Computer Technology in the city of Pravets, Bulgaria and this is our
+ course project. As huge fans of MMA as a sport, we decided something related to it would be a suitable theme 
+ for out project as it'd keep us excited and motivated. USF(UKTC Students Figthing) is a Bulgarian mixed martial arts (MMA) 
+ promotion company based in the dorms of UKTC. It is owned and operated by Martin Yordanov, Kristiyan Yordanov and Stivan Borisov.
+ It produces events in Bulgaria that showcase 8 weight divisions and abides no rules whatsoever. As of 2023 it hasn't hosted any events and is yet to
+  schedule the first one. The company is still filling its roster, which will include all three of the corporation's founders and more. <br></p>
+ <p class="italicText">Disclaimer: We do not organise real fights and do not encourage such activity in our school.</p>
         </div>
-        <div class="col-lg-9 col-6">
-            <img src="../images/Screenshot_1.png">
+        <div class="col-lg-5 col-12-pull">
+            <img src="..\images\30anniversary.jpg" alt="UKTC" height="300" title="The Vocational School of Computer Technology in the city of Pravets">
+        </div>
+    </div>
+    <div class="row" style="padding-top:5%;">
+    <div class="col-lg-4 col-6">
+            <img src="..\images\GettyImages-1416166454-1024x631.jpg" alt="Leon Edwards knocks out Kamaru Usman with head kick to win 
+            UFC welterweight title" height="300" title="Leon Edwards knocks out Kamaru Usman with head kick to win UFC welterweight title">
+    </div>
+    <div class="col-lg-8 col-6">
+    <h3 class="crimsonbg whiteText text-center" style="padding-bottom:0.25%;">What is MMA?</h3>
+    <br>
+          <p>Mixed martial arts (MMA) is a full-contact combat sport based on striking, grappling and ground fighting, incorporating 
+            techniques from various combat sports from around the world. The first documented use of the term 
+            mixed martial arts was in a review of UFC 1 by television critic Howard Rosenberg in 1993 Originally promoted as a 
+            competition to find the most effective martial arts for real unarmed combat, competitors from different fighting styles were 
+            pitted against one another in contests with relatively few rules. Later, individual fighters incorporated multiple martial 
+            arts into their style. MMA promoters were pressured to adopt additional rules to increase competitors' safety, 
+            to comply with sport regulations and to broaden mainstream acceptance of the sport. Following these changes, the sport has 
+            seen increased popularity with a pay-per-view business that rivals boxing and professional wrestling..</p>
+</div>
+<div class="row" style="padding-top:5%;padding-bottom:2%">
+        <div class="col-lg-8 col-6">
+          <h3 class="crimsonbg whiteText text-center" style="padding-bottom:0.25%;">Our goals</h3>
+          <br>
+            <p>First and foremost, our primary goal is to get an excellent grade with this project. We also want to make sure we
+              provide entertaining fights and assure the fans they'll get their money's worth. After that, our next goal would to
+              expand to multiple countries across Europe and eventually become the largest MMA promotion on the continent. We
+            want to keep the passion for the sport alive and further popularise it in our home country Bulgaria. We'd like to improve our venues
+            and expand our roster with new younger or experienced fighters and maybe even create our own MMA gyms to train new ambitious sportsmen. 
+          Our final ambition would be to conquer the market and challenge the most famouse promotions like
+          'UFC' and ' ONE Championship'.</p>
+        </div>
+        <div class="col-lg-4 col-6">
+            <img src="..\images\Screenshot_54.png" alt="BKS bate" height="300" title="Ph">
         </div>
     </div>
 </div>
-<!--<p>The team We're a group of three students from the Vocational School of Computer Technology in the city of Pravets, Bulgaria and this is our
- course project. USF(UKTC Students Figthing) is a Bulgarian mixed martial arts (MMA) promotion company based in the dorms of UKTC.
- It is owned and operated by Martin Yordanov, Kristiyan Yordanov and Stivan Borisov.
- It produces events in Bulgaria that showcase 8 weight divisions and abides no rules whatsoever.</p>..-->
     <footer>
       <?php 
       include "../components/footer.html" 
