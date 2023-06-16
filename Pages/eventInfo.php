c1c30040ce770378b5210a873ee9024c08f051ed
MartoKartechnicata <marti170705@gmail.com>
2023-06-16 21:56:34 +0300

    ok ok ok

--- Pages/eventInfo.php
+++ Pages/eventInfo.php
@@ -26,10 +26,11 @@ $allFights=mysqli_query($connection, "Select fight.id from fight join event on f
     <meta name="description" content="Events page of the USF - UKTC STUDENT FIGHTS">
     <meta name="keywords" content="usf, uktc, fights, mma, mma promotions">
     <meta name="author" content="Martin Yordanov 19315, Kristiyan Yordanov 19313, Stivan Borisov 19321">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-    <link rel="stylesheet" type="text/css" href="../Style.css?v2=1234">
+    <link rel="stylesheet" type="text/css" href="../Style.css">
+
 </head>
 <body>
     <header>
@@ -37,34 +38,34 @@ $allFights=mysqli_query($connection, "Select fight.id from fight join event on f
     include "../components/header.html" 
     ?>
     </header>
-</body>
-
-</html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Event</title>
-	  
-  </head>
-  <body>
-
-  <form method="POST">
   <?php 
-
 while ($row = $allFights->fetch_assoc()){
-  ?>
-<p><?php
 $f1=mysqli_query($connection, "Select * from fighter join fight on fighter1id=fighter.id where fight.id='{$row["id"]}'");
 $fighter1=$f1->fetch_assoc();
 $f2=mysqli_query($connection, "Select * from fighter join fight on fighter2id=fighter.id where fight.id='{$row["id"]}'");
-$fighter2=$f2->fetch_assoc();
-echo $fighter1["firstName"]." ".$fighter1["lastName"]." vs ".$fighter2["firstName"]." ".$fighter2["lastName"];?> </p>
+$fighter2=$f2->fetch_assoc();?>
+
+<div class="container-fluid events-container ">
+<div class="d-md-block d-none">
+  <div class="row">
+    <div class="col-3">
+    <img src="../images/<?php echo $fighter1['picture_name']?>" class="events-fighter-picture" alt="<?php echo $fighter1["firstName"]." ".$fighter1["lastName"]?>">
+    </div>
+    <div class="col-6 text-center">
+      <p class="fighter-names"><?php echo $fighter1["firstName"]." ".$fighter1["lastName"]." vs ".$fighter2["firstName"]." ".$fighter2["lastName"];?> </p>
+    </div>
+    <div class="col-3 fighter-2-align">
+    <img src="../images/<?php echo $fighter2['picture_name']?>" class="events-fighter-picture" alt="<?php echo $fighter2["firstName"]." ".$fighter2["lastName"]?>">
+    </div>
+  </div>
+  </div>
+</div>
+
+
 <?php
 // close while loop 
 }
 ?>
-    </form>
     <br>
     <footer class="position-static bottom-0 start-0 end-0">
       <?php 
