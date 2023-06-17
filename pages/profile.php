52ba155a549e9bef0f256f7edadafaee864ba1c0
MartoKartechnicata <marti170705@gmail.com>
2023-06-18 01:19:49 +0300

    profile/your tickets/ e gotov

--- pages/profile.php
+++ pages/profile.php
@@ -41,38 +41,42 @@ $date = new DateTime();
     ?>
   </header>
   <main>
-    <div class="container-fluid">
+    <div class="container-fluid pb-0">
   <div style="display: flex; align-items: center;">
-    <i class="fas fa-user fa-3x"></i>
+    <span class="fas fa-user fa-3x"></span>
     <div class="container-fluid">
   <div class="row">
-    <h4><?php echo $_SESSION['firstName']." ".$_SESSION['lastName']; ?></h4>
+    <h4 class="tickets-names"><?php echo $_SESSION['firstName']." ".$_SESSION['lastName']; ?></h4>
   </div>
   <div class="row">
-    <h6><?php echo $_SESSION['email']?></h6>
+    <h6 class="tickets-email"><?php echo $_SESSION['email']?></h6>
   </div>
   </div>
   </div>
+  <hr noshade style="margin-top:0px; padding-top:0px;">
+  <h1 class="text-center pb-3" >YOUR TICKETS</h1>
   <?php while ($row = $accountTickets->fetch_assoc()){ 
     $event=mysqli_query($connection, "Select event.* from event join ticket on event.id=eventId where event.id={$row['eventId']}");
     $event=$event->fetch_assoc(); 
     $eventDate = new DateTime($event["date"]);
     if($eventDate>$date || $eventDate->format('Y-m-d')==$date->format('Y-m-d')){
     ?>
-    <div class="container-fluid events-container ">
-<div class="d-md-block d-none">
-    <div class="row" style="align-items: center;">
-      <div class="col">
-<h4>Event:<?php echo $event['name'];?></h4>
-    </div>
+    <div class="container-fluid tickets-container ">
+    <div class="row">
     <div class="col">
-  <p>Date:<?php echo $event['date'];?>
-  <br>Place:<?php echo $event['place'];?></p>
+    <h4 class="event-name-ticket"><?php echo $event['name'];?></h4>
+    <p><span class="fa-solid fa-calendar-days" style="color: #000000; padding-left:0.25%; padding-right:0.25%;"></span> <?php echo $event['date'];?>
+    <br><span class="fa-solid fa-clock" style="color: #000000;"></span> 00:00
+    <br><span class="fa-solid fa-location-dot" style="color: #000000;padding-left:0.25%; padding-right:0.25%;"></span> <?php echo $event['place'];?>
+    </p>
     </div>
-    <div class="col">
-    <h4>Ticket Type:<?php echo $row['type'];?></h4>
+    <div class="col ticket-type-col text-center">
+    <p class="ticket-type-text"><?php echo $row['type'];?></p>
+    <p class="ticket-type-text-2">TICKET</p>
     </div>
   </div>
+  <div class="row">
+    <p class="ticket-information">You don't own a ticket for the event yet. The tickets you book on our website are only a reservation as in they guarantee that you will be able to buy one at our locations. To claim your tickets for the event, you have to do so at the location of the event on the day of the event. You can purchase your tickets as early as the morning of the event and until the last fight of the event is over. In order to prove that you have booked your tickets, you will be asked to show your profile on the website at the pay desk. If you need to cancel your reservation, you need to do so at least 48 hours before the event by contacting us through the information provided on the contacts page of our website.</p>
   </div>
   </div>
 <?php
