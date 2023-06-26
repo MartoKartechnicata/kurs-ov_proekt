64b4c730226e953de82f850b6bdbcaa432557abe
MartoKartechnicata <marti170705@gmail.com>
2023-06-27 01:31:28 +0300

    home-pc

--- Pages/homepage.php
+++ Pages/homepage.php
@@ -33,14 +33,40 @@ session_start();
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="../Style.css">
 </head>
-
-<body class="home-background">
-  <div class="container home-container">
+<header>
+  <div class="container-fluid">
     <div class="row">
       <div class="col">
-      <p class="home-header">WELCOME TO THE OFFICIAL HOME OF THE USF</p>
+        <img class="img-fluid logo-home" style="margin-top:3px" src="../images/logo1-red.png" alt="logo">
+      </div>
+      <div class="col">
+        <h1 class="slogan-home">We make the big fights happen</h1>
       </div>
     </div>
+  </div>
+</header>
+<body class="home-background">
+  <div class="container-fluid">
+    <div class="row row-bg home-container">
+      <div class="col text-center home-text-container" style="margin-left: 0.5rem;">
+        <p class="home-text">Find out about our upcoming events and book tickets to attend one of them</p>
+        <a class="home-btn btn btn-outline-light" href="events.php">Events</a>
+      </div>
+      <div class="col text-center home-text-container">
+        <p class="home-text">Find out more about who we are and how the USF brand came to be</p>
+        <a class="home-btn btn btn-outline-light" href="aboutus.php">About us</a>
+      </div>
+      <div class="col text-center home-text-container" >
+        <p class="home-text">Find out how to contact us in case you have any questions or suggestions</p>
+        <a class="home-btn btn btn-outline-light" href="contacts.php">Contacts</a>
+      </div>
+      <div class="col text-center home-text-container">
+        <p class="home-text">Log into an existing profile or register if you're new</p>
+        <a class="home-btn btn btn-outline-light" href="registration.php" style="margin-bottom:1%">Register</a><br>
+        <a class="btn btn-outline-light" href="login.php">Log In</a>
+      </div>
+    </div>
+    
   </div>
 </body>
 
